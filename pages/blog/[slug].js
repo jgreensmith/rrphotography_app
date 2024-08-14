394ae181054818c106eef56875c61e88a67a9a91
James <jgreensmith13@gmail.com>
2024-08-14 21:01:51 +0100

    sdfasdf

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -19,12 +19,12 @@ const Post = () => {
                 <h1 className={`${styles["heading"]} ${styles["blogTitle"]}`}>{ title }</h1>
                 <h3 className={styles.subTitle}>{`Rebecca Reynolds ${publishedAt}`}</h3>
                 <article>
-                    <BlockContent 
+                    {/* <BlockContent 
                         className={styles.blockContent}
                         blocks={body} 
                         projectId={sanityClient.clientConfig.projectId}
                         dataset={sanityClient.clientConfig.dataset}
-                    />
+                    /> */}
                 </article>
 
             </div>   
@@ -32,33 +32,5 @@ const Post = () => {
     )
 }
 
-// export const getServerSideProps = async (pageContext) => {
-//     const pageSlug = pageContext.query.slug;
-
-//     const query = `*[ _type == "post" && slug.current == $pageSlug ][0] {
-//         title,
-//         mainImage,
-//         publishedAt,
-//         body
-//     }`
-
-//     const post = await sanityClient.fetch(query, { pageSlug });
-
-//     if (!post) {
-//         return {
-//             props: null,
-//             notFound: true
-//         }
-//     } else {
-//         return {
-//             props: {
-//                 title: post.title,
-//                 mainImage: post.mainImage,
-//                 publishedAt: post.publishedAt,
-//                 body: post.body,
-//             }
-//         }
-//     }
-// }
 
 export default Post;
